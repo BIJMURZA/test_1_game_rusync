5be2286a4f4be7bcf469f84d96ee7c85c60d7f7f
KUROSAKI <xxxbijumrzaxxx@gmail.com>
2024-01-18 19:36:11 +0700

    create_function

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import { SafeAreaView, StyleSheet, Text, StatusBar, View, Image, TouchableOpacity } from 'react-native';
+
+
+const App = () => {
+
+}
+
+
+const styles = StyleSheet.create({
+
+})
+
+export default App;
