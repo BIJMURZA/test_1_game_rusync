7ef7f3d0e90039716ace21a4e0f42df25bb76fbb
KUROSAKI <xxxbijumrzaxxx@gmail.com>
2024-01-18 20:19:46 +0700

    create_header

--- App.tsx
+++ App.tsx
@@ -1,14 +1,33 @@
 import React, {useState} from 'react';
-import { SafeAreaView, StyleSheet, Text, StatusBar, View, Image, TouchableOpacity } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, StatusBar, View, Image, TouchableOpacity, FlatList } from 'react-native';
 
 
 const App = () => {
+    return  (
+        <>
+            <StatusBar barStyle="default" />
+            <SafeAreaView style={styles.mainContainer}>
+                <View style={styles.headerContainer}>
 
+                </View>
+            </SafeAreaView>
+        </>
+    );
 }
 
 
 const styles = StyleSheet.create({
-
+    headerContainer: {
+        backgroundColor: '#121a24',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        borderBottomWidth: 1,
+    },
+    mainContainer: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+    },
 })
 
 export default App;
